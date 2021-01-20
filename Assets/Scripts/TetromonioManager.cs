96afcf3c2e6aa4ca4607858a5e81e4025433c951
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-01-20 17:07:17 +0300

    fixed(movements)

--- Assets/Scripts/TetromonioManager.cs
+++ Assets/Scripts/TetromonioManager.cs
@@ -5,38 +5,56 @@ using UnityEngine;
 public class TetromonioManager : MonoBehaviour
 {
     [SerializeField] GameObject centerPoint;
-    private float fallFactor = 0.01f;
-    private bool canBeMoved = true;
-
+    private float fallFactor=0.01f;
+    public bool canBeSpawned { get;  set; }
 
     // Update is called once per frame
     void Update()
     {
-        Move(canBeMoved);
+        Move();
     }
 
-    private void Move(bool canBeMoved)
+    private bool IsMovementValid()
     {
-        if (!canBeMoved)
+        foreach (Transform block in centerPoint.transform)
         {
-            return;
+            if (block.position.x<=-9  || block.position.x>=12 || Mathf.Floor(block.position.y)<=-2)
+            {
+                return false;
+            }
         }
-        transform.position += new Vector3(0f, -fallFactor, 0f);
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        return true;
+    }
+
+    private void Move()
+    {
+        if (IsMovementValid())
         {
-            transform.position += Vector3.right;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left;
-        }
-        else if(Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += new Vector3(0f, -fallFactor * 5, 0f);
-        }
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            centerPoint.transform.eulerAngles -= new Vector3(0f, 0f, -90f);
+            transform.position += new Vector3(0f, -fallFactor, 0f);
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right;
+                if (!IsMovementValid())
+                {
+                    transform.position -= Vector3.right;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left;
+                if (!IsMovementValid())
+                {
+                    transform.position -= Vector3.left;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                centerPoint.transform.eulerAngles -= new Vector3(0f, 0f, 90f);
+                if (!IsMovementValid())
+                {
+                    centerPoint.transform.eulerAngles += new Vector3(0f, 0f, 90f);
+                }
+            }
         }
     }
 }
